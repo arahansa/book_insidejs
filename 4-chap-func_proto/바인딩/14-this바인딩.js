c5ebd1a4d4fcc2f32deb8616fb3f9f7bc3ee678d
최강용 <arahansa@naver.com>
2022-08-10 23:37:53 +0900

    ~110p 페이지까지 읽으면서 따라 침

---
+++ 4-chap-func_proto/바인딩/14-this바인딩.js
@@ -0,0 +1,19 @@
+
+
+var myObject = {
+    name: 'foo',
+    sayName: function(){
+        console.log(this.name);
+    }
+}
+
+
+var otherObject = {
+    name:'bar',
+}
+
+
+otherObject.sayName = myObject.sayName;
+
+myObject.sayName();
+otherObject.sayName();
---
+++ 4-chap-func_proto/바인딩/4-24_전역객체_전역변수.js
@@ -0,0 +1,11 @@
+function a(){
+    console.log('a :', a);
+}
+
+
+console.log(eval('a'))
+
+const aa = eval('a');
+console.log('aa :', aa);
+
+aa();
